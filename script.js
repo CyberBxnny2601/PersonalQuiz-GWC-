e8458291f7bb5fa5f100ec755e3e8a7d84d41277
CyberBxnny2601 <158208324+CyberBxnny2601@users.noreply.github.com>
2024-08-05 04:50:46 +0000

    pt.2 update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /*Add your JavaScript here*/
+var dogScore = 0;
+var catScore = 0;
+var questionCount = 0;
+var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q2a1 = document.getElementById("q2a1");
+var q2a2 = document.getElementById("q2a2");
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var button = document.getElementById("");
+button.addEventListener(“click”, functionName);
