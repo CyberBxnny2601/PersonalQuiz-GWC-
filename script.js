a92aeaf8f7b98101abeed171776fb33b2801b4be
CyberBxnny2601 <158208324+CyberBxnny2601@users.noreply.github.com>
2024-08-06 02:24:38 +0000

    pt.3 update (more work on java)

--- script.js
+++ script.js
@@ -9,4 +9,27 @@ var q2a2 = document.getElementById("q2a2");
 var q3a1 = document.getElementById("q3a1");
 var q3a2 = document.getElementById("q3a2");
 var button = document.getElementById("");
-button.addEventListener(“click”, functionName);
+q1a1.addEventListener(“click”, Harry);
+q1a2.addEventListener(“click”, Voldemort);
+q2a1.addEventListener(“click”, Voldemort);
+q2a2.addEventListener(“click”, Harry);
+q3a1.addEventListener(“click”, Voldemort);
+q3a2.addEventListener(“click”, Harry);
+function Harry(){
+  harryScore+=1;
+  questionCount+=1;
+  console.log("questionCount = " + questionCount + "harryScore = " + harryScore);
+}
+function Voldemort(){
+  voldemortScore+=1;
+  questionCount+=1;
+  console.log("questionCount = " + questionCount + "voldemortScore = " + voldemortScore);
+}
+if (questionCount == 3){
+  console.log("The quiz is done!")
+}
+if (harryScore > voldemortScore){
+  console.log("You are on Harry's Side! Congrats, you will fight on the side of good, your bravery and intelligence shines through!");
+} else if {
+  console.log("You are on Voldemort's Side! Congrats, you will fight on the side of evil, your evilness and lust for power are very apparent. ");
+}
